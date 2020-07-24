b0a3f97d21dc71f022b30e9dba370b1cca91fb63
Sean Brage <sean.brage@teliax.com>
2020-07-24 11:59:14 -0600

    add basic SASS

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,14 @@
 </template>
 
 <style lang="scss">
-
+ #app {
+     padding: 15px 27px;
+     margin: 0 auto;
+     width: 75vw;
+     max-width: 1100px;
+     display: flex;
+     flex-direction: column;
+     justify-content: space-around;
+     align-items: flex-start;
+ }
 </style>
